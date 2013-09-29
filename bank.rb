58bff9bbd6335dbbeef7403c9bc9070a0e0a00c5
beccasheldon <rebecca.e.sheldon@gmail.com>
2013-09-29 16:37:29 -0500

    bank.rb working, one more section to complete

--- bank.rb
+++ bank.rb
@@ -1,27 +1,40 @@
 class BankAccount
-  attr_accessor :user_name, :bank_balance, :credit_debt
+  attr_accessor :user_name, :bank_balance, :credit_debt, :deposit, :withdrawl, :credit_payment, :purchase_amount, :overdraft_fee
 
   def balance_total(bank_balance = 0, deposit, withdrawl)
     @deposit = deposit
     @withdrawl = withdrawl
     @bank_balance = bank_balance + deposit.to_i - withdrawl.to_i
-    puts "Your new balance is $#{@bank_balance}."
+    puts "Your current balance is $#{@bank_balance}."
   end
 
-  def credit(credit_debt = 0, credit_purchase, credit_payment)
-    @credit_purchase = credit_purchase.to_i
-    @credit_payment = credit_payment.to_i
+  def credit(credit_debt = 0, credit_purchase)
+    @credit_purchase = credit_purchase
+    @credit_payment = bank_balance
     @credit_debt = credit_debt + credit_purchase - credit_payment
-    puts "Your credit card balance is $#{@credit_debt}."
+    puts "Your purchases came to $#{@credit_purchase} and your payments came to $#{@credit_payment}. Your credit card balance is $#{@credit_debt}."
+  end
+
+  def overdraft(purchase_amount)
+    @overdraft_fee = 10 
+    @purchase_amount = purchase_amount
+    if @purchase_amount <= bank_balance
+      @new_bank_balance = bank_balance - purchase_amount
+      puts "Transaction Approved for $#{@purchase_amount}. New balance is $#{@new_bank_balance}."
+    else
+      @new_bank_balance = bank_balance - overdraft_fee
+      puts "Transaction Denied. Overdraft Fee is $#{@overdraft_fee}. New balance is $#{@new_bank_balance}."
+    end
   end
 
   def statement(user_name)
     @user_name = user_name
-    puts "#{@user_name}, your bank account balance is $#{@bank_balance} and your credit card total is -$#{@credit_debt}."
+    puts "#{@user_name}, your bank account balance is $#{@bank_balance} and your credit card total is $#{@credit_debt}."
   end
 end
 
 a = BankAccount.new
 a.balance_total(20, 10, 5)
-a.credit(100, 30, 50)
-a.statement("Becca Sheldon")
+a.overdraft(500)
+# a.credit(100, 30)
+# a.statement("Becca Sheldon")
